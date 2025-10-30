a408ca96766087464543749d1156dc86f7ef469c
Anshikapurohit10 <anshikapurohit4@gmail.com>
2025-10-30 21:55:45 +0530

    Add API configuration using axios

---
+++ Frontend/resumebuilder/src/api/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL:
+    import.meta.env.MODE === "production"
+      ? "https://zume-lgu7.onrender.com/api/"
+      : "http://localhost:5000/api/",
+});
+
+export default API;
