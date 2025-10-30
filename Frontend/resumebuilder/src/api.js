303ffd8abdd0e9113382ff9e032bca1d3ed1feb7
Anshikapurohit10 <anshikapurohit4@gmail.com>
2025-10-30 21:53:09 +0530

    Update api.js

--- Frontend/resumebuilder/src/api.js
+++ Frontend/resumebuilder/src/api.js
@@ -1,15 +1,11 @@
+
 import axios from "axios";
 
-export default axios.create({
-  baseURL: " http://localhost:5000/api/",
+const API = axios.create({
+  baseURL:
+    import.meta.env.MODE === "production"
+      ? "https://zume-lgu7.onrender.com/api/"
+      : "http://localhost:5000/api/",
 });
-// import axios from "axios";
 
-// const API = axios.create({
-//   baseURL:
-//     import.meta.env.MODE === "production"
-//       ? "https://zume-lgu7.onrender.com/api/"
-//       : "http://localhost:5000/api/",
-// });
-
-// export default API;
+export default API;
