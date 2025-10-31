9f172f74932a66fff7dc233950a16dc96b75eb39
Anshikapurohit10 <anshikapurohit4@gmail.com>
2025-10-31 12:37:35 +0530

    Enable MongoDB connection in db.js
    Uncommented the MongoDB connection code in db.js.

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,15 +1,4 @@
-// import mongoose from "mongoose";
 
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect("mongodb://localhost:27017/jobseeker");
-//     console.log("MongoDB connected");
-//   } catch (error) {
-//     console.error("MongoDB connection failed", error.message);
-//   }
-// };
-
-// export default connectDB;
 import mongoose from "mongoose";
 
 const connectDB = async () => {
