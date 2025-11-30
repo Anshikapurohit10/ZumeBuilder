9bbb8701312f54ce8e436fae04150ecbb56e096a
Anshikapurohit10 <anshikapurohit4@gmail.com>
2025-11-30 17:59:52 +0530

    Update CORS configuration in server.js
    Refactor CORS middleware to specify allowed origins and methods.

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,17 @@ connectDB();
 
 // Express app
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://zumebuild-jo10.onrender.com"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
