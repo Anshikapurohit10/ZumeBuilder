5beccb020cec2f15dff957365a1760ee89c84834
Anshikapurohit10 <anshikapurohit4@gmail.com>
2026-02-09 13:10:21 +0530

    Refactor API functions and update base URL
    Refactor API functions and update base URL for production.

--- Frontend/resumebuilder/src/api/api.js
+++ Frontend/resumebuilder/src/api/api.js
@@ -1,40 +1,4 @@
-// import axios from "axios";
 
-// const API= axios.create({
-//   baseURL:
-//     import.meta.env.MODE === "production"
-//       ? "https://zume-lgu7.onrender.com/api/"
-//       : "http://localhost:5000/api/",
-// });
-
-// export const uploadResumeATS = async (file) => {
-//   const fd = new FormData();
-//   fd.append("resume", file);
-
-  
-//   const res = await API.post("ats/analyze", fd, {
-//     headers: { "Content-Type": "multipart/form-data" },
-//   });
-
-//   return res; 
-// };
-// export const analyzeResume = async ({ resumeText, jdText, role }) => {
-//   return API.post("/ats/analyze", {
-//     resumeText,
-//     jdText,
-//     role,
-//   });
-// };
-// export const rewriteResume = (data) => {
-//   return API.post("ats/rewrite", data);
-// };
-// export const analyzeResumeATS = (data) =>
-//   API.post("/ats/analyze", data);
-
-// export const rewriteResumeATS = (data) =>
-//   API.post("/ats/rewrite", data);
-
-// export default API;
 import axios from "axios";
 
 const API = axios.create({
@@ -44,7 +8,7 @@ const API = axios.create({
       : "http://localhost:5000/api/",
 });
 
-// ✅ 1) Upload Resume
+
 export const uploadResumeATS = async (file) => {
   const fd = new FormData();
   fd.append("resume", file);
@@ -56,7 +20,6 @@ export const uploadResumeATS = async (file) => {
   return res;
 };
 
-// ✅ 2) Analyze Resume (Text or Role)
 export const analyzeResume = async ({ resumeText, jdText, role }) => {
   return API.post("ats/analyze", {
     resumeText,
@@ -65,7 +28,7 @@ export const analyzeResume = async ({ resumeText, jdText, role }) => {
   });
 };
 
-// ✅ 3) Rewrite Resume with AI
+
 export const rewriteResume = (data) => {
   return API.post("ats/rewrite", data);
 };
